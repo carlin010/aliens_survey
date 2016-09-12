c0cae3d94170716729b5cd71d58089b0ffb0b352
Nate Carlin <carlin010@knights.gannon.edu>
2016-09-12 17:41:37 -0400

    Update gulp to support MAMP and WAMP proxy

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,12 @@ var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var reload      = browserSync.reload;
 
+// Add your project's root directory name here
+var rootDir = 'aliens_survey'
 
 gulp.task('default', function () {
     browserSync.init({
-        server: {
-            baseDir: "./"
-        }
+        proxy: "localhost:8888/" + rootDir
     });
-
     gulp.watch("*.*").on("change", reload);
 });
